83586c9d6be38be15bb8294ba9d7d945717de0e4
petekaroon <pete_k92@hotmail.com>
2022-03-16 17:38:20 +0700

    created blockchain

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+	blockchainOne := createBlockchain(2)
+
+	blockchainOne.addBlock("Alice", "Bob", 5)
+	blockchainOne.addBlock("John", "Bob", 2)
+	blockchainOne.addBlock("Alice", "John", 3)
+
+	fmt.Println(blockchainOne.isValid())
 }
